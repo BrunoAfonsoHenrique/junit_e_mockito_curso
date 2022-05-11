fb3f27ad2ad4e7f48391a7eb950f65f8698d5143
Bruno Afonso Henrique <bruno_afonso_henrique@outlook.com>
2022-05-10 21:47:10 -0300

    Tratamento de exceção - Estratégia elegante

--- TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -3,7 +3,6 @@ package br.ce.wcaquino.servicos;
 import java.util.Date;
 
 import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
@@ -14,14 +13,12 @@ import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
-	
-	// https://junit.org/junit4/javadoc/4.12/org/junit/rules/TestRule.html
-	
+		
 	@Rule
 	public ErrorCollector errorCollector= new ErrorCollector();
 
 	@Test
-	public void testeLocacao() {
+	public void testeLocacao() throws Exception {
 
 		// Cenario
 		LocacaoService locacaoService = new LocacaoService();
@@ -29,14 +26,32 @@ public class LocacaoServiceTest {
 		Filme filme = new Filme("Harry Potter", 2, 5.0);
 
 		// Ação
-		Locacao locacao = locacaoService.alugarFilme(usuario, filme);
-
+		Locacao locacao;
+		
+		locacao = locacaoService.alugarFilme(usuario, filme);
+			
 		// Verificação
-		Assert.assertThat(locacao.getValor(), CoreMatchers.is(5.0)); // Verifique que o valor da locaçao é 5.0
-		errorCollector.checkThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.equalTo(6.0))); // Verifique que o valor da locação é igual a 5.0
-		errorCollector.checkThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.not(6.0))); // Verifique que o valor da locação não é 6.0
+		errorCollector.checkThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.equalTo(5.0))); // Verifique que o valor da locação é igual a 5.0
 		errorCollector.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true));
-		errorCollector.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), CoreMatchers.is(false));
+		errorCollector.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), CoreMatchers.is(true));
+	
+		
 	}
+	
+	//Estratégia elegante
+	@Test(expected = Exception.class)
+	public void testeLocacao_filmeSemEstoque() throws Exception {
+		
+		// Cenario
+		LocacaoService locacaoService = new LocacaoService();
+		Usuario usuario = new Usuario();
+		Filme filme = new Filme("Harry Potter", 0, 5.0);
+
+		// Ação
+		Locacao locacao;
+				
+		locacao = locacaoService.alugarFilme(usuario, filme);
+	}
+	
 
 }
